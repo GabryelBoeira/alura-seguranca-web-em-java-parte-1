8a555ea8431d70b9d446b065cab9882654d44ffa
GabryelBoeira <GabryelBoeira@users.noreply.github.com>
2024-08-04 20:25:26 -0300

    salvando senha com hash

--- alura-shows/src/br/com/alura/owasp/dao/UsuarioDaoImpl.java
+++ alura-shows/src/br/com/alura/owasp/dao/UsuarioDaoImpl.java
@@ -1,6 +1,7 @@
 package br.com.alura.owasp.dao;
 
 import br.com.alura.owasp.model.Usuario;
+import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
@@ -19,6 +20,9 @@ public class UsuarioDaoImpl implements UsuarioDao {
 	 * @param  usuario the user object to be saved
 	 */
 	public void salva(Usuario usuario) {
+
+		// criptografando a senha do usuário
+		usuario.setSenha(BCrypt.hashpw(usuario.getSenha(), BCrypt.gensalt()));
 		manager.persist(usuario);
 	}
 
@@ -32,11 +36,29 @@ public class UsuarioDaoImpl implements UsuarioDao {
 	public Usuario procuraUsuario(Usuario usuario) {
 
 		TypedQuery<Usuario> query = manager
-				.createQuery("SELECT u FROM Usuario u WHERE u.email = :email AND u.senha = :senha", Usuario.class);
+				.createQuery("SELECT u FROM Usuario u WHERE u.email = :email", Usuario.class);
 		query.setParameter("email", usuario.getEmail());
-		query.setParameter("senha", usuario.getSenha());
 
-		return query.getResultList().stream().findFirst().orElse(null);
+		// verifica se a senha do usuário bate com o hash do usuário
+		Usuario usuarioRetornado = query.getResultList().stream().findFirst().orElse(null);
+		if (validaSenhaUsuariomOHashBanco(usuario, usuarioRetornado)) {
+			return usuarioRetornado;
+		};
+
+		return null;
+	}
+
+	/**
+	 * Validates the password of a user against the hashed password stored in the database.
+	 *
+	 * @param  usuario       the user object containing the password to be validated
+	 * @param  usuarioRetornado the user object retrieved from the database
+	 * @return                true if the password matches the hashed password, false otherwise
+	 */
+	private boolean validaSenhaUsuariomOHashBanco(Usuario usuario, Usuario usuarioRetornado) {
+		if (usuarioRetornado == null) return false;
+
+		return BCrypt.checkpw(usuario.getSenha(), usuarioRetornado.getSenha());
 	}
 
 }
